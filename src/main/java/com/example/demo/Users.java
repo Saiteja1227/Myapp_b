936e45e1bd548d4d7afd99366ed2ab26e8c1a0c1
Saiteja <saiteja062005@gmail.com>
2026-02-26 14:10:27 +0530

    first commit

---
+++ src/main/java/com/example/demo/Users.java
@@ -0,0 +1,55 @@
+package com.example.demo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class Users {
+   @Id 
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Long id;
+
+   @Column(unique = true, nullable = false)
+   private String username;
+
+   @Column(unique = true, nullable = false)
+   private String email;
+   
+   @Column(nullable = false)
+   private String password;
+
+   public Users() {}
+
+   public void setUsername(String username) {
+       this.username = username;
+   }
+
+   public void setEmail(String email) {
+       this.email = email;
+   }
+
+   public void setPassword(String password) {
+       this.password = password;
+   }
+
+   public String getPassword() {
+       return this.password;
+   }
+   
+   public String getUsername() {
+       return this.username;
+   }
+   
+   public String getEmail() {
+       return this.email;
+   }
+   
+   public Long getId() {
+       return this.id;
+   }
+}
